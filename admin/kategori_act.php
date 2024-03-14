413ae8aed21c624e845a3880a4a9d72b5e9e67fc
Muhammad Zidan Fadillah <115553474+muhammadzidanfadilah@users.noreply.github.com>
2024-03-14 13:16:08 +0700

    Delete admin directory

--- admin/kategori_act.php
+++
@@ -1,6 +0,0 @@
-<?php 
-include '../koneksi.php';
-$nama  = $_POST['nama'];
-
-mysqli_query($koneksi, "insert into kategori values (NULL,'$nama')");
-header("location:kategori.php");
--- admin/transaksi_hapus.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include '../koneksi.php';
-$id = $_GET['id'];
-
-mysqli_query($koneksi, "delete from invoice where invoice_id='$id'");
-
-mysqli_query($koneksi,"delete from transaksi where transaksi_invoice='$id'");
-
-header("location:transaksi.php");
